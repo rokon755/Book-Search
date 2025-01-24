b4fc6dc19fe853bf93027861cee96c6895a01e9c
Rokon <rokonuaman@digitalfireflymarketing.com>
2025-01-25 00:49:06 +0600

    add

--- src/components/Boimela/BookDetails.jsx
+++ src/components/Boimela/BookDetails.jsx
@@ -1,6 +1,6 @@
 const BookDetails = (props) => {
   return (
-    <div >
+    <div className="pr-[15px]">
       <h2 className="font-bold text-[20px]">{props.title}</h2>
       <p className="text-[16px] text-gray-500">{props.author}</p>
     </div>
