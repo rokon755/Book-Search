ee20ff2a061f99fa4746b25c19cd059f2ce6428d
Rokon <rokonuaman@digitalfireflymarketing.com>
2025-02-02 17:40:16 +0600

    custom-hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import Boimela from "./components/Boimela/Boimela"
-import Avatar from "./practise/Lifting_The_State_Up/Avatar"
-import Form from "./practise/Form-cntrol/Form"
+import Avatar from "./study/Lifting_The_State_Up/Avatar"
+import DataFetch from "./study/DataFetchAPI/DataFetch";
+import Form from "./study/FormControl/Form"
+import Toggle from './study/Toggler/Toggle';
+import DataFromCustom from "./study/useCustomHook/dataFromCustom";
+
 
 function App() {
 
   return (
     <>
+      <DataFromCustom />
+      <DataFetch />
+      <Toggle />
       <Form />
       <Avatar />
       <Boimela />
